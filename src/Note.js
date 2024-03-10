a9134500e9fe9a27d8563ec0d7539c9274f5047c
selcher_k <selcher@juno.com>
2024-03-10 17:37:29 -0400

    Initial Commit

---
+++ src/Note.js
@@ -0,0 +1,48 @@
+import React from "react";
+/* The presentational component renders one
+list item element with two text input fields,
+one each for the note's title and description. 
+When the user inputs in these fields,
+functions that are passed to the component
+as props are utilized to update the state of the
+designated note object in the state of the
+App component  */
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+
+  const clickDelete = () => props.removeNote(props.note.id);
+
+  return (
+    <li className="note">
+      <input
+        type="text"
+        placeholder="Title"
+        className="note__title"
+        value={props.note.title}
+        onChange={updateTitle}
+      />
+
+      <textarea
+        placeholder="Description..."
+        className="note__description"
+        value={props.note.description}
+        onChange={updateDescription}
+      />
+      <span className="note__delete" onClick={clickDelete}>
+        X
+      </span>
+    </li>
+  );
+};
+
+export default Note;
